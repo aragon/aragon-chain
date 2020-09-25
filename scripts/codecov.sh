6d6ed5c69ee1250c148749d76a9435ac99d3eb5b
Federico Kunze <federico.kunze94@gmail.com>
2020-09-25 20:19:07 +0200

    try with single command

--- scripts/codecov.sh
+++ scripts/codecov.sh
@@ -1,9 +1,1 @@
-set -e
-touch ../coverage.txt
-for d in $(go list ./... | grep -v vendor); do
-    go test -mod=readonly -timeout 10m -coverprofile=profile.out -covermode=atomic $d
-    if [ -f profile.out ]; then
-        cat profile.out >> coverage.txt
-        rm profile.out
-    fi
-done
+go test ./... -mod=readonly -timeout 10m -race -coverprofile=coverage.txt -covermode=atomic -tags='ledger test_ledger_mock'
